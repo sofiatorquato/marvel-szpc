0121af4772fde9d01aa4b8e1df830ec460a13283
Sofia B. Torquato <70653917+sofiatorquato@users.noreply.github.com>
2026-04-24 11:27:01 -0300

    Delete JS Async directory

--- JS Async/main.js
+++
@@ -1,6 +0,0 @@
-async function escreverInfosPais(paisDesejado) {
-    const info = await fetch('https://restcountries.com/v3.1/name/' + paisDesejado);
-    console.log(await info.json());
-}
-
-escreverInfosPais("korea");
